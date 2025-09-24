440b7b4e35ae71023426163f000be2ed2eaaefd0
SidKaipa <162386908+SidKaipa@users.noreply.github.com>
2025-09-24 14:50:32 -0700

    added TODO

---
+++ src/data_analysis.py
@@ -0,0 +1,7 @@
+#TODO read file
+#TODO load_students(filename): Read CSV and return list of student data
+#TODO calculate_average_grade(students): Calculate and return average
+#TODO count_math_students(students): Count students in Math
+#TODO generate_report(): Create formatted report string
+#TODO save_report(report, filename): Write report to file
+#TODO main()
---
+++ src/data_analysis_functions.py
@@ -0,0 +1,6 @@
+#TODO load_data(filename): Generic loader that checks file extension
+#TODO load_csv(filename): Load CSV data (same technique as basic script)
+#TODO analyze_data(students): Return dictionary with multiple statistics
+#TODO analyze_grade_distribution(grades): Count grades by letter grade ranges
+#TODO save_results(results, filename): Save detailed report
+#TODO main()
